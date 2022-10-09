50767b9ffcd1c728736054d311a6ea61b7c81099
kikerkov <kikerkov@gmail.com>
2022-10-09 15:09:13 +0200

    Update

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -4,33 +4,73 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject rock;
-    public GameObject paper;
-    public GameObject scissors;
+    public GameObject _enemyRock;
+    public GameObject _enemyPaper;
+    public GameObject _enemyScissors;
+    public GameObject _playerRock;
+    public GameObject _playerPaper;
+    public GameObject _payerScissors;
+    public GameObject _playerWins;
+    public GameObject _playerLose;
 
-    public void GenerateAction()
+
+
+    public void EnemySymbolGenerate()
     {
         int action = Random.RandomRange(0, 3);
         if (action==0)
         {
-            rock.active = true;
-            paper.active = false;
-            scissors.active = false;
+            _enemyRock.active = true;
+            _enemyPaper.active = false;
+            _enemyScissors.active = false;
         }
         else if (action==1)
         {
-            rock.active = false;
-            paper.active = true;
-            scissors.active = false;
+            _enemyRock.active = false;
+            _enemyPaper.active = true;
+            _enemyScissors.active = false;
         }
         else
         {
-            rock.active = false;
-            paper.active = false;
-            scissors.active = true;
+            _enemyRock.active = false;
+            _enemyPaper.active = false;
+            _enemyScissors.active = true;
         }
         Debug.Log("Generate Action!!!");
 
+    }
+
+    public void PlayerSymbolGenerate()
+    {
+        int action = Random.RandomRange(0, 3);
+        if (action == 0)
+        {
+            _playerRock.active = true;
+            _playerPaper.active = false;
+            _payerScissors.active = false;
+        }
+        else if (action == 1)
+        {
+            _playerRock.active = false;
+            _playerPaper.active = true;
+            _payerScissors.active = false;
+        }
+        else
+        {
+            _playerRock.active = false;
+            _playerPaper.active = false;
+            _payerScissors.active = true;
+        }
+
+    }
+
+    public void Winner()
+    {
+   
+        //Koj e pobednikot?
+        //Kako da se definira koj e pobednik
+        //if enemy is rock and player is paper = player wins 
+ 
     }
 
     // Start is called before the first frame update
