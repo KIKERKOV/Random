d3f200434bc8b3a000f49e4cda3587b72af4b7e4
kikerkov <kikerkov@gmail.com>
2022-10-09 14:38:26 +0200

    Save Point

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject rock;
+    public GameObject paper;
+    public GameObject scissors;
+
+    public void GenerateAction()
+    {
+        int action = Random.RandomRange(0, 3);
+        if (action==0)
+        {
+            rock.active = true;
+            paper.active = false;
+            scissors.active = false;
+        }
+        else if (action==1)
+        {
+            rock.active = false;
+            paper.active = true;
+            scissors.active = false;
+        }
+        else
+        {
+            rock.active = false;
+            paper.active = false;
+            scissors.active = true;
+        }
+        Debug.Log("Generate Action!!!");
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
