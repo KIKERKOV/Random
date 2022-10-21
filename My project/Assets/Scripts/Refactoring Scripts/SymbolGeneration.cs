b947087c74038cab1b830ca95a6fd406b9280d11
kikerkov <kikerkov@gmail.com>
2022-10-21 14:34:51 +0200

    Lista kako rabote

--- My project/Assets/Scripts/Refactoring Scripts/SymbolGeneration.cs
+++ My project/Assets/Scripts/Refactoring Scripts/SymbolGeneration.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +7,13 @@ public class SymbolGeneration : MonoBehaviour
     public GameObject Paper;
     public GameObject Scissors;
 
-
+    List<GameObject> gameObjects;
     public void PlayerChoice()
     {
-        GameObject.Instantiate(Rock, new Vector3(0f, 0f, 0f), new Quaternion());
-        GameObject.Instantiate(Paper, new Vector3(1.5f, 0f, 0f), new Quaternion());
-        GameObject.Instantiate(Scissors, new Vector3(-1.5f, 0f, 0f), new Quaternion());
+        foreach (var go in gameObjects)
+            go.SetActive(false);
+        var randomElement = Random.Range(0, gameObjects.Count);
+        gameObjects[randomElement].SetActive(true);
     }
 
 
@@ -21,7 +21,18 @@ public class SymbolGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rockClone = GameObject.Instantiate(Rock, new Vector3(0f, 0f, 0f), new Quaternion());
+        var paperClone = GameObject.Instantiate(Paper, new Vector3(1.5f, 0f, 0f), new Quaternion());
+        var scissorsClone = GameObject.Instantiate(Scissors, new Vector3(-1.5f, 0f, 0f), new Quaternion());
+
+        rockClone.SetActive(false);
+        paperClone.SetActive(false);
+        scissorsClone.SetActive(false);
+
+        gameObjects = new List<GameObject>();
+        gameObjects.Add(rockClone);
+        gameObjects.Add(paperClone);
+        gameObjects.Add(scissorsClone);
     }
 
     // Update is called once per frame
