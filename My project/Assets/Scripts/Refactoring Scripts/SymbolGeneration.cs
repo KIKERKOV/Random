4f324a785d2e0338530f78a8788644ededca7463
kikerkov <kikerkov@gmail.com>
2022-10-21 13:58:19 +0200

    Refactoring Luck Combat

--- My project/Assets/Scripts/Refactoring Scripts/SymbolGeneration.cs
+++ My project/Assets/Scripts/Refactoring Scripts/SymbolGeneration.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class SymbolGeneration : MonoBehaviour
 {
+    public GameObject Rock;
+    public GameObject Paper;
+    public GameObject Scissors;
+
+
+    public void PlayerChoice()
+    {
+        GameObject.Instantiate(Rock, new Vector3(0f, 0f, 0f), new Quaternion());
+        GameObject.Instantiate(Paper, new Vector3(1.5f, 0f, 0f), new Quaternion());
+        GameObject.Instantiate(Scissors, new Vector3(-1.5f, 0f, 0f), new Quaternion());
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
