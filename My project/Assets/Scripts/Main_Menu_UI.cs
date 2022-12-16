4ceba178df588a4665153b6c02fa792642c7cff6
kikerkov <kikerkov@gmail.com>
2022-12-16 01:02:22 +0100

    Adding TMP

--- My project/Assets/Scripts/Main_Menu_UI.cs
+++ My project/Assets/Scripts/Main_Menu_UI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonManager : MonoBehaviour
+public class Main_Menu_UI : MonoBehaviour
 {
 
     public void StartGame()
